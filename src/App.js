f37c4ed248b9e63624212415adef12956e45b9c8
ymkim9302 <ymkim9302@naver.com>
2020-08-22 19:42:31 +0900

    2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
+const customer = {
+    'name': '홍길동',
+    'birthday': '961122',
+    'gender': '남자',
+    'job': '대학생'
+}
+
 class App extends Component {
   render() {
     return(
-      <Customer/>
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+
+      />
     );
   }
 }
